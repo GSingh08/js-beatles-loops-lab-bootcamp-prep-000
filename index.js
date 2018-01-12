e72f327f00ab788906e3efabaf8f686a2a5ebefb
Gurjot Singh <gurjotssidhu08@gmail.com>
2018-01-12 01:10:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 function theBeatlesPlay(ArrayM, ArrayI){
-  var myArray= [];
-  
+var myArray= [];
+const musicians = ["John Lennon", "Paul McCartney", "George Harrison", "Ringo Starr"];
+const instruments = ["Guitar", "Bass Guitar", "Lead Guitar", "Drums"];
   
 }
